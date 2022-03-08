bd82e982ddc8f716cfe3d4f13b79f4a6be7b3501
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-08 23:47:27 +0400

    task3

---
+++ sozde n herfi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Word("azerbaycan", 'a'));
+
+
+        }
+
+        static int Word(string str, char sr)
+        {
+            int count = 0;
+            int i;
+            for (i = 0; i < str.Length; i++)
+            {
+                if (str[i] == sr)
+                {
+                    count++;
+                }
+
+            }
+            return count;
+
+
+
+
+        }
+    }
+}
