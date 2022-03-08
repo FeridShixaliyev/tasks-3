7bd345312f6d832c9892d27142bad40671559a7c
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-08 23:45:39 +0400

    task1

---
+++ n sayda ededincemi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(SumArreyelements(1, 2, 3, 4, 5, 7, 8, 9));
+        }
+
+        static int SumArreyelements(params int [] arr)
+        {
+            int total = 0;
+            
+            foreach(var item in arr)
+            {
+                total += item;
+            }
+            return total;
+               
+        }
+    }
+}
